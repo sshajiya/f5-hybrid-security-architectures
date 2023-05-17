ae8ac0c8ac7f9097c04e96d7180c2a78ea7de826
sshajiya <39581520+sshajiya@users.noreply.github.com>
2023-05-17 22:51:09 +0530

    Update locals.tf

--- xc/locals.tf
+++ xc/locals.tf
@@ -1,7 +1,7 @@
 locals {
   project_prefix = data.tfe_outputs.infra.values.project_prefix
   build_suffix = data.tfe_outputs.infra.values.build_suffix
-  origin_bigip = try(data.tfe_outputs.bigip.values.bigip_public_vip, "")
+  origin_bigip = try(data.tfe_outputs.bigip[0].values.bigip_public_vip, "")
   origin_nginx = try(data.tfe_outputs.nap[0].values.external_name, data.tfe_outputs.nic[0].values.external_name, "")
   origin_server = "${coalesce(local.origin_bigip, local.origin_nginx)}"
   origin_port = try(data.tfe_outputs.nap[0].values.external_port, data.tfe_outputs.nic[0].values.external_port, "80")
