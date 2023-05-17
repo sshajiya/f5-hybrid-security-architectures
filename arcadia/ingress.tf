d566189d09b375cfd69f2df04dd464cdd145b32e
sshajiya <39581520+sshajiya@users.noreply.github.com>
2023-05-17 22:45:45 +0530

    Update ingress.tf

--- arcadia/ingress.tf
+++ arcadia/ingress.tf
@@ -6,7 +6,7 @@ resource "kubernetes_ingress_v1" "arcadia-ingress" {
   spec {
     ingress_class_name = "nginx"
     rule {
-      host = try(data.tfe_outputs.nap[0].values.external_name, data.tfe_outputs.nic[0].values.external_name, "sdc-org-nap-7f53-nginx-ingress-controller")      
+      host = try(data.tfe_outputs.nap[0].values.external_name, data.tfe_outputs.nic[0].values.external_name, "a9991535679dd4eeaa808891d6c98afb-1889258750.ap-south-1.elb.amazonaws.com")      
       #host = try(data.tfe_outputs.nap.values.external_name, data.tfe_outputs.nic.values.external_name)
       http {
         path {
