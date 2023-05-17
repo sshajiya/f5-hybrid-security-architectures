27e820e611ce7939d0d8e45e9b3e2817a097174c
sshajiya <39581520+sshajiya@users.noreply.github.com>
2023-05-17 21:40:14 +0530

    Update outputs.tf

--- nap/outputs.tf
+++ nap/outputs.tf
@@ -1,8 +1,10 @@
 output "external_name" {
-    value = data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname
+    #value = data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname
+    value = try(data.kubernetes_service_v1.nginx-service.status.0.load_balancer.0.ingress.0.hostname, null)    
 }
 output "external_port" {
-    value = data.kubernetes_service_v1.nginx-service.spec.0.port.0.port
+    #value = data.kubernetes_service_v1.nginx-service.spec.0.port.0.port
+    value = try(data.kubernetes_service_v1.nginx-service.spec.0.port.0.port, null)    
 }
 output "origin_source" {
     value = "nap"
